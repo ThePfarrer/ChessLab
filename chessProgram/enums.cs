52a1f1f3f0bda68ceeb362c45a80814d0a679031
ThePfarrer <orezpablo@gmail.com>
2018-11-20 00:52:04 +0100

    Initial

---
+++ chessProgram/enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chessProgram
+{
+   enum col { Black, White };
+
+   enum MovementResult
+    {
+        Hit, LegalMove, NoPieceOnSource, TargetOccupiedByOwnPiece, TargetOutsideBoard, Collision, IllegalMovement
+    }
+}
